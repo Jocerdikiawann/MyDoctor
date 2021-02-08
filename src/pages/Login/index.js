d6257df38bc86467c553cb395dba209e840d06b6
Jocerdikiawann <cocokcocok33@gmail.com>
2021-02-09 02:52:50 +0700

    Berhasil menambah login dengan email dan password dari Firebase

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,30 +1,86 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {showMessage} from 'react-native-flash-message';
 import {ILlogo} from '../../assets';
-import {Button, Gap, Input, Link} from '../../component';
-import {colors, fonts} from '../../utils';
+import {Button, Gap, Input, Link, Loading} from '../../component';
+import {Fire} from '../../config';
+import {colors, fonts, storeData, useForm} from '../../utils';
 
 const width_proportion = '80%';
 const Login = ({navigation}) => {
+  const [form, setForm] = useForm({
+    email: '',
+    password: '',
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const login = () => {
+    console.log('form: ', form);
+    setLoading(true);
+    Fire.auth()
+      .signInWithEmailAndPassword(form.email, form.password)
+      .then((res) => {
+        console.log('success: ', res);
+        setLoading(false);
+        Fire.database()
+          //penulisan es6
+          .ref(`users/${res.user.uid}/`)
+          .once('value')
+          .then((resDB) => {
+            console.log('data user: ', resDB.val());
+            if (resDB.val()) {
+              storeData('user', resDB.val());
+              navigation.replace('MainApp');
+            }
+          });
+      })
+      .catch((err) => {
+        console.log('error: ', err);
+        setLoading(false);
+        showMessage({
+          message: err.message,
+          type: 'default',
+          backgroundColor: colors.error,
+          color: colors.white,
+        });
+      });
+  };
+
   return (
-    <View style={styles.page}>
-      <ILlogo />
-      <Text style={styles.title}>Masuk dan mulai berkonsultasi</Text>
-      <Input label="Email Addres" />
-      <Gap height={24} />
-      <Input label="Password" />
-      <Gap height={10} />
-      <Link title="Forgot my password" size={12} />
-      <Gap height={40} />
-      <Button title="Sign In" onPress={() => navigation.replace('MainApp')} />
-      <Gap height={30} />
-      <Link
-        title="Create new account"
-        size={16}
-        align="center"
-        onPress={() => navigation.navigate('Register')}
-      />
-    </View>
+    <>
+      <View style={styles.page}>
+        <Gap height={40} />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <ILlogo />
+          <Text style={styles.title}>Masuk dan mulai berkonsultasi</Text>
+          <Input
+            label="Email Addres"
+            value={form.email}
+            onChangeText={(value) => setForm('email', value)}
+          />
+          <Gap height={24} />
+          <Input
+            label="Password"
+            value={form.password}
+            onChangeText={(value) => setForm('password', value)}
+            secureTextEntry
+          />
+          <Gap height={10} />
+          <Link title="Forgot my password" size={12} />
+          <Gap height={40} />
+          <Button title="Sign In" onPress={login} />
+          <Gap height={30} />
+          <Link
+            title="Create new account"
+            size={16}
+            align="center"
+            onPress={() => navigation.navigate('Register')}
+          />
+        </ScrollView>
+      </View>
+      {loading && <Loading />}
+    </>
   );
 };
 
@@ -32,7 +88,7 @@ export default Login;
 
 const styles = StyleSheet.create({
   page: {
-    padding: 40,
+    paddingHorizontal: 40,
     backgroundColor: colors.white,
     flex: 1,
   },
