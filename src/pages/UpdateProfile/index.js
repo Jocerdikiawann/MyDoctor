bcbec7d0d4478dda43a2460f85b15304592547d1
Jocerdikiawann <cocokcocok33@gmail.com>
2021-02-09 03:59:13 +0700

    berhasil update profile detail page dengan data dari firebase

--- src/pages/UpdateProfile/index.js
+++ src/pages/UpdateProfile/index.js
@@ -1,28 +1,78 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {showMessage} from 'react-native-flash-message';
 import {Button, Gap, Header, Input, Profile} from '../../component';
-import {colors} from '../../utils';
+import {Fire} from '../../config';
+import {colors, getData} from '../../utils';
 
 const UpdateProfile = ({navigation}) => {
+  const [profile, setProfile] = useState({
+    fullName: '',
+    profession: '',
+    email: '',
+  });
+
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    getData('user').then((res) => {
+      const data = res;
+      data.photo = {uri: res.photo};
+      setProfile(data);
+    });
+  }, []);
+
+  const update = () => {
+    console.log('profile: ', profile);
+    const data = profile;
+    data.photo = profile.photo.uri;
+    Fire.database()
+      .ref(`users/${profile.uid}/`)
+      .update(profile)
+      .then(() => {
+        console.log('success: ');
+      })
+      .catch((err) => {
+        showMessage({
+          message: err.message,
+          type: 'default',
+          backgroundColor: colors.error,
+          color: colors.white,
+        });
+      });
+  };
+
+  const changeText = (key, value) => {
+    setProfile({
+      ...profile,
+      [key]: value,
+    });
+  };
+
   return (
     <View style={styles.page}>
       <Header title="Edit Profile" onPress={() => navigation.goBack()} />
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.content}>
-          <Profile isRemove />
+          <Profile isRemove photo={profile.photo} />
           <Gap height={26} />
-          <Input label="Full Name" />
-          <Gap height={24} />
-          <Input label="Pekerjaan" />
-          <Gap height={24} />
-          <Input label="Email" />
-          <Gap height={24} />
-          <Input label="Password" />
-          <Gap height={40} />
-          <Button
-            title="Save Profile"
-            onPress={() => navigation.goBack('UserProfile')}
+          <Input
+            label="Full Name"
+            value={profile.fullName}
+            onChangeText={(value) => changeText('fullName', value)}
           />
+          <Gap height={24} />
+          <Input
+            label="Pekerjaan"
+            value={profile.profession}
+            onChangeText={(value) => changeText('profession', value)}
+          />
+          <Gap height={24} />
+          <Input label="Email" value={profile.email} disable />
+          <Gap height={24} />
+          <Input label="Password" value={password} />
+          <Gap height={40} />
+          <Button title="Save Profile" onPress={update} />
         </View>
       </ScrollView>
     </View>
